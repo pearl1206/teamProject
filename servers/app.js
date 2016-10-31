80eddb5cd0b30e06dcc1ae495dd0461d74a3ae5a
Your Name <you@example.com>
2016-10-31 22:34:40 +0800

    上传文件

---
+++ servers/app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = new express();
+
+var path = require("path");
+
+var staticpath = path.join(__dirname,"../static");
+app.use(express.static(staticpath));
+
+app.listen(80,function(){
+	console.log("open http://127.0.0.1:80");
+})
+
