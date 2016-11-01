8e3c5c89dc7ba0e8eaf447b627031606e0f4967b
Your Name <you@example.com>
2016-11-01 22:09:08 +0800

    token

--- servers/app.js
+++ servers/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = new express();
 
 var path = require("path");
+require("./token.js")(app);
 
 var staticpath = path.join(__dirname,"../static");
 app.use(express.static(staticpath));
---
+++ servers/config.js
@@ -0,0 +1,10 @@
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: 'wxcf2950be08966884',
+		//AppSecret(应用密钥微信扫一扫)
+		appsecret: '131e3f2edb6d8d43ae81abd9073563d2',
+		//Token(令牌)
+		token: 'powan'
+	};
+};
